0e0e9daf2c71cdb08b367f9e34bb0593665d300c
LahuddinLubis <lahuddinlubis@gmail.com>
2024-08-04 07:28:26 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,7 +9,7 @@ st.info('Prediksi Spesies Penguin dengan Algoritma Random Forest (Referensi:http
 # Dataset
 with st.expander('Dataset'):
   st.write('**Raw Data**')
-  df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
+  df = pd.read_csv('penguins_cleaned.csv')
   df
 
   st.write('Feature Dataset (X)')
